761e724a7e5250b5ce8e244f5361d42c3bac710a
PBWorks111 <42221002+PBWorks111@users.noreply.github.com>
2021-10-13 19:14:47 -0400

    Update problem_3.js

--- problem_3.js
+++ problem_3.js
@@ -1,3 +1,20 @@
 const clickCountButton = document.querySelector('#problem-3 #click-count');
 
 // write your code here
+count = 0;
+
+
+clickCountButton.onclick = function myFunction (){
+    count += 1;
+    function addS(num) {
+        if(num === 1) {
+            return '';
+        } else {
+            return 's';
+        }
+    }
+    let x = addS(count)
+    
+    clickCountButton.textContent = "You clicked the button " + count + " time" + x
+
+}
